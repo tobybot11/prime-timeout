7513b600018141b716e8ebf42416cf1c8dfc33a6
Tobias Ford <toby.ford@pobox.com>
2018-01-29 14:07:28 -0500

    rewinding back to the beginning to better understand this

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,63 @@
 extern crate futures;
 extern crate futures_cpupool;
+extern crate tokio_timer;
+
+use std::time::Duration;
 
 use futures::Future;
 use futures_cpupool::CpuPool;
+use tokio_timer::Timer;
 
-const BIG_PRIME: u64 = 15485867;
+// const BIG_PRIME: u64 = 15485867;
+const BIG_PRIME: u64 = 15485868;
 
 // Synchronous version
 fn main() {
+    // let pool = CpuPool::new_num_cpus();
+    // let timer = Timer::default();
+
+    // a future that resolves to Err after a timeout
+    // let timeout = timer.sleep(Duration::from_millis(1))
+    //     .then(|_| Err(()));
+
+    //     // a future that resolves to Ok with the primality result
+    // let prime = pool.spawn_fn(|| {
+    //     Ok(is_prime(BIG_PRIME))
+    // });
+
+    // a future that resolves to one of the above values -- whichever
+    // completes first!
+    // let winner = timeout.select(prime).map(|(win, _)| win);
+
+    // // now block until we have a winner, then print what happened
+    // match winner.wait() {
+    //     Ok(true) => println!("Prime"),
+    //     Ok(false) => println!("Not prime"),
+    //     Err(_) => println!("Timed out"),
+    // }
+
+    // spawn our computation, getting back a *future* of the answer
+    // let prime_future = pool.spawn_fn(|| {
+    //   let prime = is_prime(BIG_PRIME);
+
+    // For reasons we'll see later, we need to return a Result here
+    // let res: Result<bool, ()> = Ok(prime);
+    //         res
+    //   });
+
+    // println!("Created the future");
+
+    // unwrap here since we know the result is Ok
+    //     if prime_future.wait().unwrap() {
+    //       println!("Prime");
+    // } else {
+    //   println!("Not prime");
+    // }
+
     if is_prime(BIG_PRIME) {
-        println!("Prime");
+        println!("Prime!");
     } else {
-        println!("Not prime");
+        println!("Not Prime");
     }
 }
 
